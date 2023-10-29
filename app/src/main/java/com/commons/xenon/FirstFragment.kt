e7085baa0aeeb0714393d9d5e1f3458ec76e576a
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2023-10-29 03:35:49 +0100

    Welcome to Commons

--- app/src/main/java/com/commons/xenon/FirstFragment.kt
+++ app/src/main/java/com/commons/xenon/FirstFragment.kt
@@ -1,10 +1,10 @@
 package com.commons.xenon
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.commons.xenon.databinding.FragmentFirstBinding
 
@@ -22,7 +22,7 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
